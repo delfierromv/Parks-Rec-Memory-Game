82ec74115f00e7011fc2040b07344ef8551cc456
delfierromv <delfierro.mv@gmail.com>
2022-01-02 17:49:56 -0800

    Finding a match vs. Not

--- script.js
+++ script.js
@@ -69,12 +69,16 @@ document.addEventListener('DOMContentLoaded',() =>{
   ]
  
   cardArray.sort(()=> 0.3- Math.random());
+  console.log(cardArray);
  
   const grid = document.querySelector('#grid')
-  let flipped;
   let cardChosen = []
   let cardChosenId = [];
   let cardsWon = [];
+  let moves= 0;
+  const greenBlank = 'images/pnr_blank1.jpeg';
+  const movesDisplay = document.getElementById("result");
+  const moveDisplayFunc = function(){movesDisplay.textContent = `${moves}`};
 
  //CREATE BOARD function
 
@@ -82,7 +86,7 @@ function createBoard(){
   for(let i = 0; i< cardArray.length;i++){
     let card = document.createElement('img');
     //setting each card image to a blank card (the flipped over position)
-    card.setAttribute('src','images/pnr_blank1.jpeg');
+    card.setAttribute('src',`${greenBlank}`);
     //giving each card a data id--data id's are useful when wanting to store data that is constantly changing and also when wanting to store extra information on standard elements without having to use hacks that you would need to use with non-standard attributes.
     card.setAttribute('data-id', i); //this gives the element a data-id between 0 and cardArray.length -1
     console.log(card);
@@ -95,11 +99,37 @@ function createBoard(){
 
 // CHECK FOR MATCHES
 function checkMatch(){
-  if (cardChosen[1]===cardChosen[2]){
-    
+  let flippedCard;
+  console.log(cardChosen[0]);
+  console.log(cardChosen[1]);
+  console.log(cardChosen);
+  //FINDING A MATCH
+  if (cardChosen[0]===cardChosen[1]){
+    //"remove" card from game
+    cardChosen.map((_,i)=>{
+      flippedCard = document.querySelector(`[data-id='${cardChosenId[i]}']`);
+      flippedCard.setAttribute('src','images/white_blank.jpg');
+      flippedCard.style.padding= "0";
+    });
+    //ALERT RIGHT ANSWER
+    alert('You got it right!');
+    // ADD MOVE TO MOVES
+    moves++;
+    moveDisplayFunc();
+    // Clear out cardChosen & cardChosenId array by popping cards into cardsWon
+    cardChosen = [];
+    cardChosenId = [];
+  } 
+  //NOT FINDING A MATCH
+  else if(cardChosen[0]!== cardChosen[1]){
+    //FLIP CARDS BACK OVER
+    flippedCard = document.querySelector(`[data-id='${cardChosenId[i]}']`);
+    flippedCard.setAttribute('src','images/pnr_blank1');
+    alert('wrong');
   }
-  cardChosen = [];
-  cardChosenId = [];
+  else{
+    console.log('error');
+  }
 }
 
 // FLIP YOUR CARD
@@ -108,13 +138,13 @@ function flipCard (){
   let cardId = this.getAttribute('data-id');
   cardChosen.push(cardArray[cardId].name);
   cardChosenId.push(cardId);
-  if (cardChosen ===2){
+  if (cardChosen.length ===2){
     setTimeout(checkMatch, 200);
   };
   console.log(cardChosen);
   console.log(cardChosenId);
   // SHOWING THE GIF
-  flipped = document.querySelector(`[data-id='${cardId}']`);
+  let flipped = document.querySelector(`[data-id='${cardId}']`);
   flipped.setAttribute('src', cardArray[cardId].img);
 }
 
